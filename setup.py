5183edf01b2df33ac1efa9d415c0f2ed3d789678
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2022-07-15 11:30:33 +0200

    Fix(scripts): Move b&f to python install location
    b&f needs to be sourced in .bashrc too to have the same pid as bash
    within they are called when `$$` executed

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ from dirstory.constants import VERSION
 if __name__ == "__main__":
     setup(
         version=VERSION,
-        package_data={"": ["scripts/_dirstorypatch"]},
+        package_data={"": ["scripts/_dirstorypatch", "scripts/b", "scripts/f"]},
         include_package_data=True,
     )
